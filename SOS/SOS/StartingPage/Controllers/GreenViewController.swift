4850c9923bd777ce397df24accbb1e8d2795044b
Mukhamed Khasanza <fragilehm2411@gmail.com>
2017-10-21 07:52:25 +0600

    MainPage designed

---
+++ SOS/SOS/StartingPage/Controllers/GreenViewController.swift
@@ -0,0 +1,57 @@
+//
+//  GreenViewController.swift
+//  SOS
+//
+//  Created by Khasanza on 10/21/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import AVFoundation
+class GreenViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var greenView: UIView! {
+        didSet {
+            greenView.layer.masksToBounds = true
+            greenView.layer.cornerRadius = 100
+        }
+    }
+    var locationManager: CLLocationManager!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        AVAudioSession.sharedInstance().requestRecordPermission () {
+            [unowned self] allowed in
+            if allowed {
+                // Microphone allowed, do what you like!
+                
+            } else {
+                // User denied microphone. Tell them off!
+                
+            }
+        }
+        //locationManager.delegate = self
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        // Do any additional setup after loading the view.
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    // do stuff
+                    print("hello")
+                }
+            }
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            print(location.coordinate)
+        }
+    }
+
+}
