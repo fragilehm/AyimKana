cf3f252011a553d0d5cb6d16ba5bef8faf60846a
Khasanza Mukhamed <fragilehm2411@gmail.com>
2017-10-22 14:02:09 +0600

    documentation added

---
+++ SOS/SOS/HandBook/Cells/HandBookTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HandBookTableViewCell.swift
+//  SOS
+//
+//  Created by Khasanza on 10/22/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import UIKit
+
+class HandBookTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SOS/SOS/Models/Article.swift
@@ -0,0 +1,9 @@
+//
+//  Article.swift
+//  SOS
+//
+//  Created by Khasanza on 10/22/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import Foundation
