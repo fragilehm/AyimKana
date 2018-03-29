e51aff5ea94fbf895a91712df6f648c2c572585a
mamadnazar <shohkarim7@gmail.com>
2018-03-29 21:52:49 +0600

    added AboutApp

---
+++ SOS/SOS/AboutApp/AboutAppViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AboutAppViewController.swift
+//  SOS
+//
+//  Created by ITLabAdmin on 3/29/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import UIKit
+
+class AboutAppViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        self.title = "AboutApp"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+}
